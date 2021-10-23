b98c82e3e4abb99d41325e67e7b84ddf3ef3ff4c
Thomas Min <thomas.min.git@gmail.com>
2021-10-23 20:28:30 +0900

    feat: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  export let name: string;
+  import Button from "@smui/button";
+
+  let counter = 0;
+  function handleIncreaseClicked() {
+    counter++;
+  }
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  <Button on:click={handleIncreaseClicked}>Increase</Button>
+  <p>Counter: {counter}</p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
